1481bbc2792f7b5d2f28650daa3aa9dc6f3e4abe
Zak Estrada <zestrad2@illinois.edu>
2014-02-25 16:58:21 -0600

    Finally got the bit counting working

--- task_2.R
+++ task_2.R
@@ -1,11 +1,13 @@
 #!/usr/bin/env Rscript
 source('load_data.R')
+library("BMS") #requires install.packages("BMS")
 
 data <- load_data() 
 #Looks like we have the syndromes corresponding to x8, so let's assume that's
 #what we have
 #2.13.1 (page 246)
 syndromes <- read.table('./data/ECC_correctable_syndromes.txt', header=TRUE, sep=' ')
+#syndromes <- read.table('./data/Table131_x4_ECC_Correctable_syndromes', header=TRUE, sep=' ', skip_col=1)
 
 
 #Part 1 - Breakdown of the memory errors % in Single, dual, triple,
@@ -17,9 +19,16 @@ for (level in levels(data$Syndrome)) {
   #I gave up on regex capture
   #synd <- regexpr("=\\s+(?<synd>\\w+)", level, perl=TRUE)[[1]]
   synd <- strsplit(level,"= ")[[1]][2]
-  if(nchar(synd)==4) {
-    print(synd)
+  #For 2, 3, 4 bit errors
+  if(nchar(synd)>=3) {
+    #Fix 3 character syndromes for hex2bin
+    if(nchar(synd)==3) {
+      synd<-paste('0',synd,sep='')
+    }
     #look it up in the syndrome table, based on the row we can tell how many 
     #bits are in error - if the mask has one bit, etc...
+    loc<-which(syndromes==toupper(synd),arr.ind=TRUE)
+    bin<-hex2bin(sprintf('%x',loc[1]))
+    numbits = length(grep('1',bin))
   }
 }
