e56dc55c7ee62519967c1464ceb6d260c03dd74b
Zak Estrada <zestrad2@illinois.edu>
2014-04-01 10:58:03 -0500

    Moved miniproject to subdir

--- load_data.R
+++
@@ -1,8 +0,0 @@
-# Function used to load the data into memory in R.
-# Could be viewed as part of Task 0, Item 2.
-load_data <- function() {
-  data1 <- read.table('data/machineCheckData_week_40.txt', header=TRUE, sep='\t')
-  data2 <- read.table('data/machineCheckData_week_41.txt', header=TRUE, sep='\t')
-  data <- rbind(data1, data2)
-  data
-}
