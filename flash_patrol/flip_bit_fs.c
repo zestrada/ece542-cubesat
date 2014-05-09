0ce9c1697717431274e9231b2c881fbd4aaeffb3
Zachary <zak@mustang0.illinois.edu>
2014-05-09 16:26:27 -0500

    Made simple fault injector

---
+++ flash_patrol/flip_bit_fs.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+
+int main(int argc, char *argv[]) {
+  int fd;
+  off_t off=0;
+  char buf;
+  size_t nbytes;
+
+  //Worst. argument. parsing. ever
+  fd = open(argv[1],O_RDWR); 
+  if(fd < 0) {
+    perror("no open");
+    return errno;
+  }
+
+  if(argc>2) {
+    off=atoi(argv[2]);
+  }
+
+  if(lseek(fd, off, SEEK_SET)<0) {
+    perror("no seek");
+    return errno;
+  }
+  
+  read(fd, &buf, 1);
+  printf("Read %c at pos %d\n", buf, off);
+  buf^=0x01;
+  nbytes = write(fd, &buf, 1);
+
+  if(nbytes>0) {
+    printf("Wrote %c at pos %d\n", buf, (int) off);
+  } else {
+    perror("no write");
+    return errno;
+  }
+  return 0;
+}
