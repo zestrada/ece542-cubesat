9cf5d3f565634ccfc4dad7923afb62dc18e2028c
Zachary <zestrad2@illinois.edu>
2014-05-09 22:01:57 -0500

    added data from run1 of flash patrol

--- flash_patrol/fault_inject-cubesat.sh
+++ flash_patrol/fault_inject-cubesat.sh
@@ -10,5 +10,5 @@ find /home/zak/cubesat/data/ -type f -exec cp {} /mnt/inotify/ \;
 sudo ~/clear_cache.sh  
 sleep 2
 RAND=`shuf -i 0-${NBYTES} -n 1`
-#sudo ./flip /dev/sdb1 $RAND
+sudo ./flip /dev/sdb1 $RAND
 sudo ~/clear_cache.sh  
